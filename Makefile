4d66b1ab4699fdef37093dd9e3439d12d17c82ef
Louis Narmour <lnarmour@rams.colostate.edu>
2020-03-18 11:24:09 -0600

    gemm
    '-O3 -no-vec' gives ~55 sec (N=5000)
    '-O3 -xcore-avx2' gives ~12 sec (N=5000)

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ MM: $(OBJS) ss-wrapper.c
 	$(CC) ss-wrapper.c $(OBJS) -o MM $(CFLAGS) -D$(PRECISION)=1
 
 ss.o: ss.c
-	$(CC) ss.c -c -o ss.o $(CFLAGS) $(DEFS) -D$(PRECISION)=1 -ftree-vectorize -mavx2 -fopt-info-all=opt.info
+	$(CC) ss.c -c -o ss.o $(CFLAGS) $(DEFS) -D$(PRECISION)=1 -qopt-report-phase=vec -qopt-report=5
 
 clean:
 	rm -f *.o MM
---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+commit="$(git rev-parse --short HEAD)"
+flags=(-no-vec -xhost -xavx -xcore-avx2 -march=broadwell -march=haswell -march=ivybridge)
+
+for flag in ${flags[@]};
+do
+  desc="$(echo $flag | sed 's~-march=~~' | sed 's~-xcore~~' | sed 's~-~~g')"
+  make -B OPTS="-O3 ${flag}"
+  mv MM "GEMM.$(hostname).${desc}.unaligned.${commit}"
+  mv ss.optrpt "GEMM.optrpt.$(hostname).${desc}.unaligned.${commit}"
+done;
+
