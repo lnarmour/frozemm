f3f37766bda7df60ef485021612d38d30ae336b1
Louis Narmour <lnarmour@rams.colostate.edu>
2020-03-18 13:33:20 -0600

    gather script

--- gather.sh
+++ gather.sh
@@ -3,21 +3,34 @@
 machine=$(hostname)
 log="${machine}.log"
 
-flags=(novec xhost broadwell haswell ivybridge xavx avx2)
-Ns=(2000 5000 7000)
+kernels=(GEMM MM);
+flags=(novec xhost broadwell haswell ivybridge xavx avx2);
+Ns=(2000 5000 7000);
 
-rm -rf ${log}
+rm -rf ${log};
 
-for N in ${Ns[@]};
+for kernel in ${kernels[@]};
 do
-  for flag in ${flags[@]};
+  for N in ${Ns[@]};
   do
-    printf "${N},${machine},${flag}," >> ${log};
-    for r in {1..5};
+    for flag in ${flags[@]};
     do
-      time="$(./bin/GEMM.${machine}.${flag}.aligned.f2fe92d ${N})";
-      printf "${time}," >> ${log};
-    done;
-    printf "\n" >> ${log};
-  done
+      printf "${kernel},${N},${machine},${flag}," >> ${log};
+      for r in {1..5};
+      do
+        if [ "${kernel}" == "GEMM" ]; then
+          commit="f2fe92d";
+        elif [ "${kernel}" == "MM" ]; then
+          commit="ec0c401";
+        fi
+        time="$(./bin/${kernel}.${machine}.${flag}.aligned.${commit} ${N})";
+        if [ -z "${time}" ]; then
+          printf "illegal instruction,,,,," >> ${log};
+          break;
+        fi
+        printf "${time}," >> ${log};
+      done;
+      printf "\n" >> ${log};
+    done
+  done;
 done;
