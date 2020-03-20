e42b3f81aa33b216525fb6748a38464e881701fb
Louis Narmour <lnarmour@rams.colostate.edu>
2020-03-20 14:08:31 -0600

    fma example

--- benchmark/fma.c
+++ benchmark/fma.c
@@ -9,10 +9,6 @@ int posix_memalign(void **memptr, size_t alignment, size_t size);
 #define start_timer() gettimeofday(&time, NULL); elapsed_time = (((double) time.tv_sec) + ((double) time.tv_usec)/1000000)
 #define stop_timer() gettimeofday(&time, NULL); elapsed_time = (((double) time.tv_sec) + ((double) time.tv_usec)/1000000) - elapsed_time
 
-#ifndef N
-#define N 1000
-#endif
-
 static void * xmalloc (size_t num)
 {
   void* new = NULL;
@@ -30,35 +26,25 @@ void kernel(float *restrict x0, float *restrict y0, float *restrict z0,
 {
   int i,j,k;
   for (i=0; i<N; i++) 
-    for (j=0; j<N; j++) {
-      // #pragma vector aligned
+    for (j=0; j<N; j++) 
       for (k=0; k<N; k++) {
         z0[k] += x0[k] * y0[k];
         z1[k] += x1[k] * y1[k];
       }
-    }
 }
 
 void main(int argc, char *argv[])
 {
-  // x, y, & z should all fit in maxline L1d cache (32K bytes) if N < 1250
-  float *x0 = (float*)xmalloc(sizeof(float) * N); // 4N KB
-  float *y0 = (float*)xmalloc(sizeof(float) * N); // 4N KB
-  float *z0 = (float*)xmalloc(sizeof(float) * N); // 4N KB
-  float *x1 = (float*)xmalloc(sizeof(float) * N); // 4N KB
-  float *y1 = (float*)xmalloc(sizeof(float) * N); // 4N KB
-  float *z1 = (float*)xmalloc(sizeof(float) * N); // 4N KB
+  float *x0 = (float*)xmalloc(sizeof(float) * N);
+  float *y0 = (float*)xmalloc(sizeof(float) * N);
+  float *z0 = (float*)xmalloc(sizeof(float) * N);
+  float *x1 = (float*)xmalloc(sizeof(float) * N);
+  float *y1 = (float*)xmalloc(sizeof(float) * N);
+  float *z1 = (float*)xmalloc(sizeof(float) * N);
 
   start_timer();
   kernel(x0,y0,z0,x1,y1,z1);
   stop_timer();
   
   printf("%f\n", elapsed_time);
-
-
-
-  if (atoi(argv[0]) == 9999999) {
-    for (int i=0; i<N; i++) 
-      printf("%f,%f,%f%f,%f,%f\n",x0[i],y0[i],z0[i],x1[i],y1[i],z1[i]);
-  }
 }
