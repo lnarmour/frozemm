a983eaf7b6fb693675ff7b0f01fc8c8d2b2b6a38
Louis Narmour <lnarmour@rams.colostate.edu>
2020-03-23 22:45:04 -0600

    works

--- benchmark/gen_versions.py
+++ benchmark/gen_versions.py
@@ -6,12 +6,9 @@ def main():
     j_loop = '    for (j=0; j<N; j+={}) {{'
     k_loop = '      for (k=0; k<N; k+={}) {{'
 
-    UNROLL = {'i': [1,2,4,8], 'j': [1,2,4,8], 'k': [1,2,4,8]}
+    UNROLL = {'i': [1,2], 'j': [1,2], 'k': [1,2]}
     idxs = [['i.i', 'i.j', 'i.k', 'j.i', 'j.j', 'j.k', 'k.i', 'k.j', 'k.k']]*3
     PERMS = ['.'.join(list(e)) for e in itertools.product(*idxs)]
-
-    #UNROLL = {'i': [1,2], 'j': [1], 'k': [1,2]}
-    #PERMS = PERMS[:2]
 
     b_cnt = 0
 
@@ -21,9 +18,6 @@ def main():
                 for unroll_i in UNROLL['i']:
                     for unroll_j in UNROLL['j']:
                         for unroll_k in UNROLL['k']:
-                            if perm != 'i.i.i.j.k.j' and perm != 'i.i.i.j.k.j':
-                                continue
-
                             print('  #ifdef version{}'.format(b_cnt))
                             print('  // shared_writes({}) shared_reads({}) unroll({},{},{}) perm({})'.format(shared_writes, shared_reads, unroll_i, unroll_j, unroll_k, perm))
 
@@ -44,49 +38,25 @@ def main():
 
 
 def gen_body(shared_reads, shared_writes, unroll_i, unroll_j, unroll_k, perm):
-    z0 = 'z0'
-    z1 = 'z0' if shared_writes else 'z1'
-    x0 = 'x0'
-    x1 = 'x0' if shared_reads else 'x1'
-    y0 = 'y0'
-    y1 = 'y1'
+    z0, x0, y0 = 'z0', 'x0', 'y0'
+    z1, x1, y1 = 'z0' if shared_writes else 'z1', 'x0' if shared_reads else 'x1', 'y1'
+    l0, l1, l2, l3, l4, l5 = perm.split('.')
 
     for ui in range(unroll_i):
         for uj in range(unroll_j):
             for uk in range(unroll_k):
-                for idx in ['i', 'j', 'k']:
-                    # C = (l0+c0)*d1+(l1+c1)
-                    # A = (l2+c2)*d1+(l3+c3)
-                    # B = (l4+c4)*d1+(l5+c5)
-                    #
-                    # for each lX, X is either 'i', 'j', or 'k'
-                    # cX goes from range(0,uX)
+                cX = {'i': ui, 'j': uj, 'k': uk}
+                c = [cX[l] for l in perm.split('.')]
 
-                    l0,l1,l2,l3,l4,l5 = perm.split('.')
-                    cX = {'i': ui, 'j': uj, 'k': uk}
-    
-                    S0_LHS =  '{}[({}+{{}})*N+({}+{{}})]'.format(z0, l0, l1)
-                    S0_RHS0 = '{}[({}+{{}})*N+({}+{{}})]'.format(x0, l2, l3)
-                    S0_RHS1 = '{}[({}+{{}})*N+({}+{{}})]'.format(y0, l4, l5)
-                    
-                    print(idx,'>', S0_LHS)   
-                    print(idx,'>', S0_RHS0)   
-                    print(idx,'>', S0_RHS1)   
+                S0_LHS =  '{}[({}+{})*N+({}+{})]'.format(z0, l0, c[0], l1, c[1])
+                S0_RHS0 = '{}[({}+{})*N+({}+{})]'.format(x0, l2, c[2], l3, c[3])
+                S0_RHS1 = '{}[({}+{})*N+({}+{})]'.format(y0, l4, c[4], l5, c[5])
+                S1_LHS = '{}[({}+{})*N+({}+{})]'.format(z1, l0, c[0], l1, c[1])
+                S1_RHS0 = '{}[({}+{})*N+({}+{})]'.format(x1, l2, c[2], l3, c[3])
+                S1_RHS1 = '{}[({}+{})*N+({}+{})]'.format(y1, l4, c[4], l5, c[5])
 
- 
-                    # S0_LHS = z0[(i+{})*N+(i+{})]
-
-                    S0_LHS = S0_LHS.format(cX[idx] if idx==l0 or idx==l1 else 0, '{}')
-                    S0_LHS = S0_LHS.format(cX[idx] if idx==l0 or idx==l1 else 0, '{}')
-                    S0_RHS0 = S0_RHS0.format(cX[idx] if idx==l2 or idx==l3 else 0, '{}')
-                    S0_RHS0 = S0_RHS0.format(cX[idx] if idx==l2 or idx==l3 else 0, '{}')
-                    S0_RHS1 = S0_RHS1.format(cX[idx] if idx==l4 or idx==l5 else 0, '{}')
-                    S0_RHS1 = S0_RHS1.format(cX[idx] if idx==l4 or idx==l5 else 0, '{}')
-
-
-                    if idx in perm: 
-                        print(idx, '{}{} += {} * {};'.format(' '*8, S0_LHS, S0_RHS0, S0_RHS1))
-
+                print('        {} += {} * {};'.format(S0_LHS, S0_RHS0, S0_RHS1))
+                print('        {} += {} * {};'.format(S1_LHS, S1_RHS0, S1_RHS1))
 
 if __name__ == '__main__':
     main()
