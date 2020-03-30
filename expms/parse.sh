e212b2296ec20dfa03aa13cd15a63c934884f992
Louis Narmour <lnarmour@rams.colostate.edu>
2020-03-30 11:19:57 -0600

    update parse

--- expms/parse.sh
+++ expms/parse.sh
@@ -2,16 +2,22 @@
 
 TK=({5000..2000..-1000} {1500..500..-500} {400..200..-100} {175..25..-25})
 
+FILE=$1
+if [ -z "$1" ]; then
+  echo "Usage: ./parse.sh RESULTS_FILE";
+  exit 1;
+fi
+
 for tk in ${TK[@]}; 
 do 
-  if [ -z "$(cat results.log | grep -A10 "TK=$tk)" | grep '^.\+\.')" ]; then
+  if [ -z "$(cat $FILE | grep -A10 "TK=$tk)" | grep '^.\+\.')" ]; then
     continue
   fi
-  t="$(cat results.log | grep -A10 "TK=$tk)" | grep '^.\+\.' | python utils/mean.py)";
-  std="$(cat results.log | grep -A10 "TK=$tk)" | grep '^.\+\.' | python utils/std.py)";
-  offcore_reqs="$(cat results.log | grep -A20 "TK=$tk)" | grep 'OFFCORE' | sed 's~^[ ]\+\([0-9]\+,.*\)      r.*~\1~' | sed 's~,~~g' )";
-  L3_miss="$(cat results.log | grep -A21 "TK=$tk)" | grep 'L3_MISS' | sed 's~^[ ]\+\([0-9]\+,.*\)      r.*~\1~' | sed 's~,~~g' )";
-  cmd="$(cat results.log | grep "TK=$tk)")"
+  t="$(cat $FILE | grep -A10 "TK=$tk)" | grep '^.\+\.' | python utils/mean.py)";
+  std="$(cat $FILE | grep -A10 "TK=$tk)" | grep '^.\+\.' | python utils/std.py)";
+  offcore_reqs="$(cat $FILE | grep -A20 "TK=$tk)" | grep 'OFFCORE' | sed 's~^[ ]\+\([0-9]\+,.*\)      r.*~\1~' | sed 's~,~~g' )";
+  L3_miss="$(cat $FILE | grep -A21 "TK=$tk)" | grep 'L3_MISS' | sed 's~^[ ]\+\([0-9]\+,.*\)      r.*~\1~' | sed 's~,~~g' )";
+  cmd="$(cat $FILE | grep "TK=$tk)")"
   N="$(echo $cmd | sed 's~./MM \([0-9]\+\) .*~\1~')"
   PI="$(echo $cmd | sed 's~./MM.*PI=\([0-9]\+\) .*~\1~')"
   PJ="$(echo $cmd | sed 's~./MM.*PJ=\([0-9]\+\) .*~\1~')"
