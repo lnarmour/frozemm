e1c12c30f63109ab2fca1327486896419cf0ebf8
Louis Narmour <lnarmour@rams.colostate.edu>
2020-04-01 20:58:51 -0600

    exp save

--- expms/parse.sh
+++ expms/parse.sh
@@ -8,19 +8,33 @@ if [ -z "$1" ]; then
   exit 1;
 fi
 
-for tk in ${TK[@]}; 
-do 
-  if [ -z "$(cat $FILE | grep -A10 "TK=$tk)" | grep '^.\+\.')" ]; then
-    continue
-  fi
-  t="$(cat $FILE | grep -A10 "TK=$tk)" | grep '^.\+\.' | python utils/mean.py)";
-  std="$(cat $FILE | grep -A10 "TK=$tk)" | grep '^.\+\.' | python utils/std.py)";
-  offcore_reqs="$(cat $FILE | grep -A20 "TK=$tk)" | grep 'OFFCORE' | sed 's~^[ ]\+\([0-9]\+,.*\)      r.*~\1~' | sed 's~,~~g' )";
-  L3_miss="$(cat $FILE | grep -A21 "TK=$tk)" | grep 'L3_MISS' | sed 's~^[ ]\+\([0-9]\+,.*\)      r.*~\1~' | sed 's~,~~g' )";
-  cmd="$(cat $FILE | grep "TK=$tk)")"
-  N="$(echo $cmd | sed 's~./MM \([0-9]\+\) .*~\1~')"
-  PI="$(echo $cmd | sed 's~./MM.*PI=\([0-9]\+\) .*~\1~')"
-  PJ="$(echo $cmd | sed 's~./MM.*PJ=\([0-9]\+\) .*~\1~')"
+PI=(10000 7500 5000 2500)
 
-  echo "$N,$PI,$PJ,$tk,$offcore_reqs,$L3_miss,$t,$std"
-done;
+for pi in ${PI[@]};
+do 
+  if [[ "$pi" == "10000" ]]; then
+    TK=(10000)
+  elif [[ "$pi" == "7500" ]]; then
+    TK=({7500..2500..-1000} {2000..500..-500} {400..200..-100} {175..25..-25})
+  elif [[ "$pi" == "5000" ]]; then
+    TK=({5000..2000..-1000} {1500..500..-500} {400..200..-100} {175..25..-25})
+  elif [[ "$pi" == "2500" ]]; then
+    TK=({2500..500..-500} {400..200..-100} {175..25..-25})
+  fi
+
+  for tk in ${TK[@]}; 
+  do 
+    if [ -z "$(cat $FILE | grep -A12 "PI=$pi" | grep -A10 "TK=$tk)" | grep '^.\+\.')" ]; then
+      continue
+    fi
+    t="$(cat $FILE | grep -A12 "PI=$pi" | grep -A5 "TK=$tk)" | grep '^.\+\.' | python utils/mean.py)";
+    std="$(cat $FILE | grep -A12 "PI=$pi" | grep -A5 "TK=$tk)" | grep '^.\+\.' | python utils/std.py)";
+    L3_miss="$(cat $FILE | grep -A12 "PI=$pi" | grep -A16 "TK=$tk)" | grep 'L3_MISS' | sed 's~^[ ]\+\([0-9]\+,.*\)      ANY.*~\1~' | sed 's~,~~g' )";
+    cmd="$(cat $FILE | grep -A12 "PI=$pi" | grep "TK=$tk)")"
+    N="$(echo $cmd | sed 's~./MM \([0-9]\+\) .*~\1~')"
+    PI="$(echo $cmd | sed 's~./MM.*PI=\([0-9]\+\) .*~\1~')"
+    PJ="$(echo $cmd | sed 's~./MM.*PJ=\([0-9]\+\) .*~\1~')"
+  
+    echo "$N,$PI,$PJ,$tk,$L3_miss,$t,$std"
+  done;
+done
