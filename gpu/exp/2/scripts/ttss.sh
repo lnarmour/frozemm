cb3e9b8aea96077c8b76afb13422f42ed9e4fb39
Louis Narmour <lnarmour@rams.colostate.edu>
2020-06-25 08:32:36 -0600

    script save

--- gpu/exp/2/scripts/ttss.sh
+++ gpu/exp/2/scripts/ttss.sh
@@ -1,12 +1,45 @@
-for P in {20..16..-2}; do 
-  eval "for TK in {$(($P))..1..-1}; 
-    do tp=\$(($P*1024)); 
-    tk=\$((\$TK*128*8)); 
-    for r in {1..3}; 
-    do 
-      echo ./bin/sgemm-ttss \$tp \$tp \$tp \$tk; 
-#      ./bin/sgemm-ttss \$tp \$tp \$tp \$tk;
-    done; 
-    echo ""; 
-  done;"; 
-done;
+#!/bin/bash
+
+N=10000;
+
+if [ -n "$JOULES" ]; then
+  for r in {1..3};
+  do
+    c="./bin/sgemm-ttss $N $N $N $N";
+    echo $c;
+    eval $c;
+  done;
+  echo "";
+  
+  for P in {500..5000..500}; 
+  do 
+    for TK in {100..1000..100};
+    do
+      for r in {1..3};
+      do
+        c="./bin/sgemm-ttss $N $P $P $TK";
+        echo $c;
+        eval $c;
+      done;
+      echo "";
+    done;
+  done;
+fi;
+
+if [ -n "$NVPROF" ]; then
+  c="lnvprof --profile-from-start off -m dram_read_transactions,dram_write_transactions bin/sgemm-ttss $N $N $N $N";
+  echo $c;
+  eval $c;
+  echo "";
+
+  for P in {500..5000..500}; 
+  do 
+    for TK in {100..1000..100};
+    do
+      c="lnvprof --profile-from-start off -m dram_read_transactions,dram_write_transactions bin/sgemm-ttss $N $P $P $TK";
+      echo $c;
+      eval $c;
+      echo "";
+    done;
+  done;
+fi;
