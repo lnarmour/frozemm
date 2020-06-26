8a5552bbb1b3425ee915489d5a576b6d84714d32
Louis Narmour <lnarmour@rams.colostate.edu>
2020-06-26 08:18:23 -0600

    remove comments

--- gpu/exp/3/tmp/matmultKernel01.cu
+++ gpu/exp/3/tmp/matmultKernel01.cu
@@ -1,50 +1,18 @@
-///
-/// matmultKernel00.cu
-/// For CSU CS575 Spring 2011
-/// Instructor: Wim Bohm
-/// Based on code from the CUDA Programming Guide
-/// Modified by Wim Bohm and David Newman
-/// Created: 2011-01-27
-/// Last Modified: 2011-02-23 DVN
-///
-/// Multiplies two matrices using CUDA: A x B = C
-///
-/// Copy this file and modify the MatMultKernel device function for
-/// each of your experiments. 
-///
-
 #include "matmultKernel.h"
 #include <stdio.h>
 
-// Define a gpu kernel to perform matrix multiplication
-// of A x B = C.
 __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
   float *Asub, *Bsub, *Csub;
   int num_threads=BLOCK_SIZE_X*BLOCK_SIZE_Y;
   Csub = &C.elements[C.stride * STRIP_SIZE * blockIdx.y + FOOTPRINT_SIZE_X * blockIdx.x];
-
-  // Each thread computes one element of Csub in its copy of CValue
-  //int total_values = (FOOTPRINT_SIZE/BLOCK_SIZE)*(FOOTPRINT_SIZE/BLOCK_SIZE);
+  
   float Cvalues[SCALING_FACTOR] = {[0 ... SCALING_FACTOR-1]=0};
-  //for (int c=0; c < SCALING_FACTOR1; ++c){
-    //     Cvalues[c]=0;      
-  // }
- 
-  // Loop over all sub matrices in blockIdx.y of A and blockIdx.x of B
-  // required to compute Csub. Block multiply each pair of sub matrices
-  // and accumulate results
+  
   for (int m = 0;  m < (A.width / STRIP_SIZE); ++m){
-    // Get Asub and Bsub descriptors
+    
     Asub = &A.elements[A.stride * STRIP_SIZE * m + FOOTPRINT_SIZE_Y * blockIdx.y];
     Bsub = &B.elements[B.stride * STRIP_SIZE * m + FOOTPRINT_SIZE_X * blockIdx.x];
-    // Copy ELEMENTS OF  ASub and Bsub into shared memory
-    // EACH THREAD loads ONE ELEMENT of ASub and ONE of Bsub
-    // Notice: it does not need to be the element it requires to
-    //         compute its Cvalue, as long as all elements are 
-    //         collaboratively read. 
-
-    // Notice: every thread declares shared_A and shared_B in shared memory
-    //         even though a thread block has only one shared_A and one shared_B
+    
     __shared__ float shared_A[FOOTPRINT_SIZE_Y][STRIP_SIZE];
     __shared__ float shared_B[STRIP_SIZE][FOOTPRINT_SIZE_X];
     
@@ -55,11 +23,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
 	}
      }
     __syncthreads();
-
-     // Synchronize to ensure all elements are read
     
-    // Do an inproduct of one row of shared_A and one col of shared_B
-    // computing one Cvalue by accumulation
     if (MODE == 0){
 	int c=0;
 	#pragma unroll
