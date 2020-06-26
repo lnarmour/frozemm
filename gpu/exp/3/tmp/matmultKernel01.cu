1700a1e2bbf87eb7b17439dde16874bffa629ba0
Louis Narmour <lnarmour@rams.colostate.edu>
2020-06-26 08:16:33 -0600

    simplify

--- gpu/exp/3/tmp/matmultKernel01.cu
+++ gpu/exp/3/tmp/matmultKernel01.cu
@@ -20,12 +20,8 @@
 // of A x B = C.
 __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
   float *Asub, *Bsub, *Csub;
-  int thread_row = threadIdx.y;
-  int thread_col = threadIdx.x;
-  int block_row = blockIdx.y;
-  int block_col = blockIdx.x;
   int num_threads=BLOCK_SIZE_X*BLOCK_SIZE_Y;
-  Csub = &C.elements[C.stride * STRIP_SIZE * block_row + FOOTPRINT_SIZE_X * block_col];
+  Csub = &C.elements[C.stride * STRIP_SIZE * blockIdx.y + FOOTPRINT_SIZE_X * blockIdx.x];
 
   // Each thread computes one element of Csub in its copy of CValue
   //int total_values = (FOOTPRINT_SIZE/BLOCK_SIZE)*(FOOTPRINT_SIZE/BLOCK_SIZE);
@@ -34,13 +30,13 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
     //     Cvalues[c]=0;      
   // }
  
-  // Loop over all sub matrices in block_row of A and block_col of B
+  // Loop over all sub matrices in blockIdx.y of A and blockIdx.x of B
   // required to compute Csub. Block multiply each pair of sub matrices
   // and accumulate results
   for (int m = 0;  m < (A.width / STRIP_SIZE); ++m){
     // Get Asub and Bsub descriptors
-    Asub = &A.elements[A.stride * STRIP_SIZE * m + FOOTPRINT_SIZE_Y * block_row];
-    Bsub = &B.elements[B.stride * STRIP_SIZE * m + FOOTPRINT_SIZE_X * block_col];
+    Asub = &A.elements[A.stride * STRIP_SIZE * m + FOOTPRINT_SIZE_Y * blockIdx.y];
+    Bsub = &B.elements[B.stride * STRIP_SIZE * m + FOOTPRINT_SIZE_X * blockIdx.x];
     // Copy ELEMENTS OF  ASub and Bsub into shared memory
     // EACH THREAD loads ONE ELEMENT of ASub and ONE of Bsub
     // Notice: it does not need to be the element it requires to
@@ -54,8 +50,8 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
     
     for (int i=0; i<STRIP_SIZE; i=i+BLOCK_SIZE_Y){
 	for (int j=0; j<FOOTPRINT_SIZE_X; j = j + BLOCK_SIZE_X){
-		shared_A[thread_col + j][thread_row + i]=Asub[(thread_row + i)*A.stride + (thread_col + j)];
-		shared_B[thread_row + i][thread_col + j]=Bsub[(thread_row + i)*B.stride + (thread_col + j)];
+		shared_A[threadIdx.x + j][threadIdx.y + i]=Asub[(threadIdx.y + i)*A.stride + (threadIdx.x + j)];
+		shared_B[threadIdx.y + i][threadIdx.x + j]=Bsub[(threadIdx.y + i)*B.stride + (threadIdx.x + j)];
 	}
      }
     __syncthreads();
@@ -72,7 +68,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
 			for (int l=0; l<BLOCK_SIZE_Y; ++l){
 				for (int k=0; k<BLOCK_SIZE_X; ++k){
 					for (int e=0; e<STRIP_SIZE; ++e){
-						Cvalues[c] += shared_A[(thread_row * BLOCK_SIZE_Y)+i+l][e] * shared_B[e][(thread_col * BLOCK_SIZE_X)+j+k];
+						Cvalues[c] += shared_A[(threadIdx.y * BLOCK_SIZE_Y)+i+l][e] * shared_B[e][(threadIdx.x * BLOCK_SIZE_X)+j+k];
 					}
 					c=c+1;
 				}
@@ -87,7 +83,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
    	for (int i=0; i<STRIP_SIZE; i=i+(BLOCK_SIZE_X)){
         	for (int j=0; j<FOOTPRINT_SIZE_X; j=j+(BLOCK_SIZE_X)){
             		for (int e=0; e<STRIP_SIZE; ++e){
-                		Cvalues[c] += shared_A[thread_row + i][e] * shared_B[e][thread_col + j];
+                		Cvalues[c] += shared_A[threadIdx.y + i][e] * shared_B[e][threadIdx.x + j];
 			}
 			c=c+1;
 		}
@@ -102,7 +98,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
 			for (int l=0; l<SCALING_FACTOR_X; ++l){
 				for (int k=0; k<SCALING_FACTOR_Y; ++k){
 					for (int e=0; e <STRIP_SIZE; ++e){
-						Cvalues[c] += shared_A[thread_row*SCALING_FACTOR_Y + i + l][e] * shared_B[e][thread_col*SCALING_FACTOR_X + j + k];
+						Cvalues[c] += shared_A[threadIdx.y*SCALING_FACTOR_Y + i + l][e] * shared_B[e][threadIdx.x*SCALING_FACTOR_X + j + k];
 					}
 					c=c+1;
 				}
@@ -118,7 +114,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
 		for (int j=0; j<FOOTPRINT_SIZE_X; j=j+(BLOCK_SIZE_X*num_threads)){
 			for (int l=0; l<BLOCK_SIZE_Y; ++l){
 				for (int k=0; k<BLOCK_SIZE_X; ++k){
-					Csub[((thread_row*BLOCK_SIZE_Y)+i+l)*C.stride + (thread_col*BLOCK_SIZE_X)+j+k] = Cvalues[d];
+					Csub[((threadIdx.y*BLOCK_SIZE_Y)+i+l)*C.stride + (threadIdx.x*BLOCK_SIZE_X)+j+k] = Cvalues[d];
 					d=d+1;
 				}
 			}
@@ -128,7 +124,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
     if (MODE == 1){ 
 	for (int i=0; i<STRIP_SIZE; i=i+(BLOCK_SIZE_X)){
 		for (int j=0; j<FOOTPRINT_SIZE_X; j=j+(BLOCK_SIZE_X)){
-			Csub[(thread_row + i) * C.stride + (thread_col+j)] = Cvalues[d];
+			Csub[(threadIdx.y + i) * C.stride + (threadIdx.x+j)] = Cvalues[d];
 			d=d+1;
 		}
 	}
@@ -138,7 +134,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
 		for (int j=0; j<FOOTPRINT_SIZE_X; j=j+(SCALING_FACTOR_X*num_threads)){
 			for (int l=0; l<SCALING_FACTOR_X;++l){
 				for (int k=0; k<SCALING_FACTOR_Y;++k){
-					Csub[(thread_row*SCALING_FACTOR_Y + i + l)*C.stride+(thread_col*SCALING_FACTOR_X+j)+k]=Cvalues[d];
+					Csub[(threadIdx.y*SCALING_FACTOR_Y + i + l)*C.stride+(threadIdx.x*SCALING_FACTOR_X+j)+k]=Cvalues[d];
 					d=d+1;
 				}
 			}
