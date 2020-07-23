3cec798438358e4e4530b6bfbdb9ff8d52aa5d78
Louis Narmour <lnarmour@rams.colostate.edu>
2020-07-23 08:26:23 -0600

    wrapper to gen cuda files from input C stencil

---
+++ gpu/an5d/scripts/gen_cuda.sh
@@ -0,0 +1,34 @@
+#!/usr/local/bin/bash
+
+# given a C stencil, and AN5D configuration parameters (bs1, bt, sl)
+# generate the cuda source files
+if [[ -z "$(which an5d)" ]]; then
+  echo "error: an5d tool not installed";
+  exit 1;
+fi
+
+if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
+  echo "usage: $0 STENCIL_C BS1 BT SL";
+  exit 1;
+fi
+
+bs1=$2
+bt=$3
+sl=$4
+
+an5d --bs1=$bs1 --bt=$bt --sl=$sl $1
+
+stencil=`echo $1 | sed 's~\(.*\)\..*~\1~'`
+
+if [[ ! -f "${stencil}_kernel.cu" ]]; then
+  echo "an5d code generation failed";
+  exit 1;
+fi
+
+sed -i '' "s~\(#include \"${stencil}\)\(_kernel.hu\"\)~\1-${bs1}-${bt}-${sl}\2~" ${stencil}_host.cu
+sed -i '' "s~\(#include \"${stencil}\)\(_kernel.hu\"\)~\1-${bs1}-${bt}-${sl}\2~" ${stencil}_kernel.cu
+
+mv ${stencil}_host.cu ${stencil}-${bs1}-${bt}-${sl}_host.cu
+mv ${stencil}_kernel.cu ${stencil}-${bs1}-${bt}-${sl}_kernel.cu
+mv ${stencil}_kernel.hu ${stencil}-${bs1}-${bt}-${sl}_kernel.hu
+
