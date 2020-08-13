2af29a6ddd53db89d4e0e3c0662dc4830449afd5
Louis Narmour <lnarmour@rams.colostate.edu>
2020-08-13 12:18:40 -0600

    mp poc sed: sed -i 's~A\[\(.*t.*\)\]\[\(.*i.*\)\]\[\(.*j.*\)\]\[\(.*k.*\)\]~A[\1][\2-BENCH_RAD*t][\3-BENCH_RAD*t][\4-BENCH_RAD*t]~' star3d2r.c

--- gpu/multipass/csrc/star3d2r.c
+++ gpu/multipass/csrc/star3d2r.c
@@ -24,15 +24,15 @@ double kernel_stencil(SB_TYPE *A1, int compsize, int timestep, bool scop)
       for (int i = pi*PI + BENCH_RAD; i < (pi+1)*PI - BENCH_RAD; i++)
         for (int j = pj*PJ + BENCH_RAD; j < (pj+1)*PJ - BENCH_RAD; j++)
           for (int k = pk*PK + BENCH_RAD; k < (pk+1)*PK - BENCH_RAD; k++)
-            A[(t+1)%2][i][j][k] =
-              0.2500f * A[t%2][i][j][k]
-              + 0.0620f * A[t%2][i-1][j][k] + 0.0621f * A[t%2][i+1][j][k]
-              + 0.0622f * A[t%2][i][j-1][k] + 0.0623f * A[t%2][i][j+1][k]
-              + 0.0624f * A[t%2][i][j][k-1] + 0.06245f * A[t%2][i][j][k+1]
+            A[(t+1)%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t] =
+              0.2500f * A[t%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0620f * A[t%2][i-1][j][k] + 0.0621f * A[t%2][i+1-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0622f * A[t%2][i][j-1][k] + 0.0623f * A[t%2][i-BENCH_RAD*t][j+1-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0624f * A[t%2][i][j][k-1] + 0.06245f * A[t%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k+1-BENCH_RAD*t]
 
-              + 0.06255f * A[t%2][i-2][j][k] + 0.0626f * A[t%2][i+2][j][k]
-              + 0.0627f  * A[t%2][i][j-2][k] + 0.0628f * A[t%2][i][j+2][k]
-              + 0.0629f  * A[t%2][i][j][k-2] + 0.0630f * A[t%2][i][j][k+2];
+              + 0.06255f * A[t%2][i-2][j][k] + 0.0626f * A[t%2][i+2-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0627f  * A[t%2][i][j-2][k] + 0.0628f * A[t%2][i-BENCH_RAD*t][j+2-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0629f  * A[t%2][i][j][k-2] + 0.0630f * A[t%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k+2-BENCH_RAD*t];
 #pragma endscop
   }
   else {
@@ -41,15 +41,15 @@ double kernel_stencil(SB_TYPE *A1, int compsize, int timestep, bool scop)
       for (int i = BENCH_RAD; i < dimsize - BENCH_RAD; i++)
         for (int j = BENCH_RAD; j < dimsize - BENCH_RAD; j++)
           for (int k = BENCH_RAD; k < dimsize - BENCH_RAD; k++)
-            A[(t+1)%2][i][j][k] =
-              0.2500f * A[t%2][i][j][k]
-              + 0.0620f * A[t%2][i-1][j][k] + 0.0621f * A[t%2][i+1][j][k]
-              + 0.0622f * A[t%2][i][j-1][k] + 0.0623f * A[t%2][i][j+1][k]
-              + 0.0624f * A[t%2][i][j][k-1] + 0.06245f * A[t%2][i][j][k+1]
+            A[(t+1)%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t] =
+              0.2500f * A[t%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0620f * A[t%2][i-1][j][k] + 0.0621f * A[t%2][i+1-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0622f * A[t%2][i][j-1][k] + 0.0623f * A[t%2][i-BENCH_RAD*t][j+1-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0624f * A[t%2][i][j][k-1] + 0.06245f * A[t%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k+1-BENCH_RAD*t]
 
-              + 0.06255f * A[t%2][i-2][j][k] + 0.0626f * A[t%2][i+2][j][k]
-              + 0.0627f  * A[t%2][i][j-2][k] + 0.0628f * A[t%2][i][j+2][k]
-              + 0.0629f  * A[t%2][i][j][k-2] + 0.0630f * A[t%2][i][j][k+2];
+              + 0.06255f * A[t%2][i-2][j][k] + 0.0626f * A[t%2][i+2-BENCH_RAD*t][j-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0627f  * A[t%2][i][j-2][k] + 0.0628f * A[t%2][i-BENCH_RAD*t][j+2-BENCH_RAD*t][k-BENCH_RAD*t]
+              + 0.0629f  * A[t%2][i][j][k-2] + 0.0630f * A[t%2][i-BENCH_RAD*t][j-BENCH_RAD*t][k+2-BENCH_RAD*t];
   }
 
   return (((end_time != 0.0) ? end_time : sb_time()) - start_time);
