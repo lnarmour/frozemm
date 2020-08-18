d15201b4189a94303af2f547fc9b6e1e1e11bf1d
Louis Narmour <lnarmour@rams.colostate.edu>
2020-08-18 12:57:03 -0600

    star3d2r-mp-32x32-2-64.P100

---
+++ gpu/multipass/csrc/ts/star3d2r-mp-32x32-2-64.P100.c
@@ -0,0 +1,54 @@
+#define BENCH_DIM 3
+#define BENCH_FPP 25
+#define BENCH_RAD 2
+
+#include "common.h"
+
+#define P 100
+
+
+double kernel_stencil(SB_TYPE *A1, int compsize, int timestep, bool scop)
+{
+  double start_time = sb_time(), end_time = 0.0;
+  int dimsize = compsize + BENCH_RAD * 2;
+  SB_TYPE (*A)[dimsize][dimsize][dimsize]
+    = (SB_TYPE (*)[dimsize][dimsize][dimsize])A1;
+
+  int p = 1;
+
+  if (scop) {
+#pragma scop
+    for (int t = 0; t < timestep; t++)
+      for (int i = p*P; i < (p+1)*P; i++)
+        for (int j = p*P; j < (p+1)*P; j++)
+          for (int k = p*P; k < (p+1)*P; k++)
+            A[(t+1)%2][i][j][k] =
+              0.2500f * A[t%2][i][j][k]
+              + 0.0620f * A[t%2][i-1][j][k] + 0.0621f * A[t%2][i+1][j][k]
+              + 0.0622f * A[t%2][i][j-1][k] + 0.0623f * A[t%2][i][j+1][k]
+              + 0.0624f * A[t%2][i][j][k-1] + 0.06245f * A[t%2][i][j][k+1]
+
+              + 0.06255f * A[t%2][i-2][j][k] + 0.0626f * A[t%2][i+2][j][k]
+              + 0.0627f  * A[t%2][i][j-2][k] + 0.0628f * A[t%2][i][j+2][k]
+              + 0.0629f  * A[t%2][i][j][k-2] + 0.0630f * A[t%2][i][j][k+2];
+#pragma endscop
+  }
+  else {
+    for (int t = 0; t < timestep; t++)
+#pragma omp parallel for
+      for (int i = p*P; i < (p+1)*P; i++)
+        for (int j = p*P; j < (p+1)*P; j++)
+          for (int k = p*P; k < (p+1)*P; k++)
+            A[(t+1)%2][i][j][k] =
+              0.2500f * A[t%2][i][j][k]
+              + 0.0620f * A[t%2][i-1][j][k] + 0.0621f * A[t%2][i+1][j][k]
+              + 0.0622f * A[t%2][i][j-1][k] + 0.0623f * A[t%2][i][j+1][k]
+              + 0.0624f * A[t%2][i][j][k-1] + 0.06245f * A[t%2][i][j][k+1]
+
+              + 0.06255f * A[t%2][i-2][j][k] + 0.0626f * A[t%2][i+2][j][k]
+              + 0.0627f  * A[t%2][i][j-2][k] + 0.0628f * A[t%2][i][j+2][k]
+              + 0.0629f  * A[t%2][i][j][k-2] + 0.0630f * A[t%2][i][j][k+2];
+  }
+
+  return (((end_time != 0.0) ? end_time : sb_time()) - start_time);
+}
